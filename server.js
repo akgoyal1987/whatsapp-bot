6c2cc61abd908ac8a3bca2fd96e3b52e26b051c9
ankit_javadeveloper <ak.goyal1987@gmail.com>
2022-05-24 17:38:33 +0530

    Using config objet for static values.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const verify_token = 'blue_panda';
+const messagesHandler = require('./whatsapp-api').messagesHandler;
+const config = require('./config.json');
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
@@ -10,10 +10,20 @@ app.get("/", function (request, response) {
     response.send('Simple WhatsApp Webhook tester</br>There is no front-end, see server.js for implementation!');
 });
 
-app.post("/webhook", function (request, response) {
-    console.log('Incoming webhook: ' + JSON.stringify(request.body));
-    console.log(JSON.stringify(request.body));
-    response.sendStatus(200);
+app.post("/webhook", async function (req, res) {
+    if (req.body.object) {
+        if (req.body.entry && req.body.entry[0].changes && req.body.entry[0].changes[0].value.messages && req.body.entry[0].changes[0].value.messages[0]) {
+            let from = req.body.entry[0].changes[0].value.messages[0].from;
+            let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body;
+            const response = await messagesHandler(from, msg_body);
+            // console.log({response});
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(400);
+        }
+    } else {
+        res.sendStatus(400);
+    }
 });
 
 app.get("/webhook", function (request, response) {
@@ -26,7 +36,7 @@ app.get("/webhook", function (request, response) {
     // Check if a token and mode were sent
     if (mode && token) {
         // Check the mode and token sent are correct
-        if (mode === "subscribe" && token === verify_token) {
+        if (mode === "subscribe" && token === config.verify_token) {
             // Respond with 200 OK and challenge token from the request
             console.log("WEBHOOK_VERIFIED");
             response.status(200).send(challenge);
@@ -38,6 +48,10 @@ app.get("/webhook", function (request, response) {
         response.sendStatus(403);
     }
 });
+
+app.get('*', function(req, res) {
+    res.sendStatus(404);
+})
 
 var listener = app.listen(process.env.PORT || 3000, function () {
     console.log('Your app is listening on port ' + listener.address().port);
