c2287b12f49acc394f6787eb4a5a9ea1a42f2512
ankit_javadeveloper <ak.goyal1987@gmail.com>
2022-07-10 00:18:45 +0530

    handled media

--- src/whatsapp-routes.js
+++ src/whatsapp-routes.js
@@ -3,35 +3,35 @@ const whatsappAPI = require("../whatsappapi");
 const config = require("../config.json");
 const router = express.Router();
 
-router.get('/', async (request, response) => {
-    console.log("webhook get request");
-    // Parse params from the webhook verification request
-    let mode = request.query["hub.mode"];
-    let token = request.query["hub.verify_token"];
-    let challenge = request.query["hub.challenge"];
+router.get('/', async (req, res) => {
+    console.log("webhook get req");
+    // Parse params from the webhook verification req
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
     if (mode && token) {
         if (mode === "subscribe" && token === config.verify_token) {
             console.log("WEBHOOK_VERIFIED");
-            response.status(200).send(challenge);
+            res.status(200).send(challenge);
         } else {
-            response.sendStatus(403);
+            res.sendStatus(403);
         }
     } else {
-        response.sendStatus(403);
+        res.sendStatus(403);
     }
 })
 
-router.post('/', async (request, response) => {
+router.post('/', async (req, res) => {
     if (req.body.object) {
         if (req.body.entry && req.body.entry[0].changes && req.body.entry[0].changes[0].value.messages && req.body.entry[0].changes[0].value.messages[0]) {
             let msgType = req.body.entry[0].changes[0].value.messages[0].type;
             try {
                 if (msgType === 'text') {
-                    const response = await whatsappAPI.handleTextMessage(req.body.entry[0].changes[0].value.messages);
+                    await whatsappAPI.handleTextMessage(req.body.entry[0].changes[0].value.messages);
                 } else if (msgType === 'button') {
-                    const response = await whatsappAPI.handleButtonMessage(req.body.entry[0].changes[0].value.messages);
+                    await whatsappAPI.handleButtonMessage(req.body.entry[0].changes[0].value.messages);
                 }  else if (msgType === 'document' || msgType === 'image') {
-                    const response = await whatsappAPI.handleMediaMessage(req.body.entry[0].changes[0].value.messages);
+                    await whatsappAPI.handleMediaMessage(req.body.entry[0].changes[0].value.messages);
                 }
                 res.sendStatus(200);
             } catch (error) {
